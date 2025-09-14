f99d81fd60e1600bf3abb5292ba14b882a768334
Nitin Chaudhary <nitin.io@outlook.com>
2025-09-14 22:03:13 +0530

    update main.py to accept path argument

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from curses.ascii import isalpha
 from stats import get_num_words, get_character_counts, get_sorted_list_of_char
-
+import sys
 def main():
-	path_to_file = 'books/frankenstein.txt'
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	path_to_file = sys.argv[1]
+
 	num_words = get_num_words(path_to_file)
 	char_dict = get_character_counts(path_to_file)
 	sorted_list = get_sorted_list_of_char(char_dict)
 	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
+	print(f"Analyzing book found at {path_to_file}...")
 	print("----------- Word Count ----------")
 	print(f"Found {num_words} total words")
 	print("--------- Character Count -------")
